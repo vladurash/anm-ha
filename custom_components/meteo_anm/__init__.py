3728fb21908b6c280c86036b5323c8ac6472d28c
Vlad <vladurash@yahoo.com>
2026-01-02 02:49:18 +0200

    implementare api-uri noi si modificare logica setare addon

---
+++ custom_components/meteo_anm/__init__.py
@@ -0,0 +1,28 @@
+DOMAIN = "meteo_anm"
+
+
+async def async_setup_entry(hass, config_entry):
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][config_entry.entry_id] = config_entry.add_update_listener(async_reload_entry)
+
+    await hass.config_entries.async_forward_entry_setups(config_entry, ["sensor"])
+    return True
+
+
+async def async_unload_entry(hass, config_entry):
+    unload_ok = await hass.config_entries.async_unload_platforms(config_entry, ["sensor"])
+
+    unsub = hass.data.get(DOMAIN, {}).pop(config_entry.entry_id, None)
+    if unsub:
+        unsub()
+    if not hass.data.get(DOMAIN):
+        hass.data.pop(DOMAIN, None)
+
+    return unload_ok
+
+async def async_setup(hass, config):
+    return True
+
+
+async def async_reload_entry(hass, entry):
+    await hass.config_entries.async_reload(entry.entry_id)
---
+++ custom_components/meteo_anm/config_flow.py
@@ -0,0 +1,52 @@
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.core import callback
+import homeassistant.helpers.config_validation as cv
+
+class AlertaANMConfigFlow(config_entries.ConfigFlow, domain="meteo_anm"):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            # Validăm datele introduse
+            update_interval = user_input.get("update_interval")
+            judet = (user_input.get("judet") or "").strip().upper()
+            localitate = user_input.get("localitate")
+            if update_interval and update_interval > 0 and judet:
+                cleaned_input = {**user_input, "judet": judet}
+                return self.async_create_entry(title="Prognoza Meteo si Avertizari by ANM", data=cleaned_input)
+            errors["base"] = "invalid_interval" if not update_interval or update_interval <= 0 else "invalid_judet"
+
+        # Formulăm schema de configurare
+        schema = vol.Schema({
+            vol.Required("update_interval", default=1800): cv.positive_int,
+            vol.Required("localitate", default="Bucuresti"): cv.string,
+            vol.Required("judet", default="B"): vol.All(cv.string, vol.Length(min=1, max=2)),
+        })
+
+        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return AlertaANMOptionsFlowHandler(config_entry)
+
+class AlertaANMOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self._config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        return await self.async_step_user()
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        schema = vol.Schema({
+            vol.Required("update_interval", default=self._config_entry.options.get("update_interval", self._config_entry.data.get("update_interval", 1800))): cv.positive_int,
+            vol.Required("localitate", default=self._config_entry.options.get("localitate", self._config_entry.data.get("localitate", "Bucuresti"))): cv.string,
+            vol.Required("judet", default=self._config_entry.options.get("judet", self._config_entry.data.get("judet", "B"))): vol.All(cv.string, vol.Length(min=1, max=2)),
+        })
+
+        return self.async_show_form(step_id="user", data_schema=schema)
